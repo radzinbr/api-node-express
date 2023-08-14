6ca8d56a04e132f2d2d2b2e013116bbfe5930696
rad <samuelcostabr@gmail.com>
2023-08-14 17:18:10 -0300

    14-08-2023

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 //const express = require('express');
 import express from 'express'
-
+import userRouter from './routes/userRouter.js'
 const api = express();
 
 
@@ -8,19 +8,7 @@ api.get('/',(req,res) => {
     res.json({message:"bem vindo a API"})
 })
 
-
-api.post('/',(req,res) => {
-    res.json({message:"metodo post"})
-})
-
-api.put('/',(req,res) => {
-    res.json({message:"metodo put"})
-})
-
-api.delete('/',(req,res) => {
-    res.json({message:"metodo delete"})
-})
-
+api.use('/users',userRouter)
 //inicializa o servidor na porta 3000
 api.listen(3000, () => {
     console.log('servidor falhou.... kkk zuera porra ta rodando kkkkk https://localhost:3000/')
